637edc36f85f4c4a2d8caf5b46aed1758bed94e4
DasEd955 <info@tunerdesign.org>
2026-04-22 00:59:15 -0400

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.py
@@ -0,0 +1,27 @@
+class TrieNode:
+    def __init__(self):
+        self.children = dict()
+        self.isWord = False
+
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+
+        root = TrieNode()
+
+        for string in strs:
+            current = root
+            for char in string:
+                if char not in current.children:
+                    current.children[char] = TrieNode()
+                current = current.children[char]
+            current.isWord = True
+        
+        current, prefix = root, str()
+        while len(current.children) == 1 and current.isWord == False:
+            key, val = next(iter(current.children.items()))
+            prefix += key
+            current = val
+        
+        return prefix
+
+        
